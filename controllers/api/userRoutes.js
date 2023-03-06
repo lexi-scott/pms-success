c5b7489c221c1a18bfe495174695a591985ea542
slexicon <lexiscott0101@gmail.com>
2023-03-05 22:38:50 -0800

    login and sign up routes and db working

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -1,55 +1,69 @@
 const router = require('express').Router();
-const User  = require('../../models/User');
+const User = require('../../models/User');
 
+router.get('/', (req, res) => {
+  // Get all user from user table
+  User.findAll().then((UserData) => {
+    res.json(UserData);
+  });
+});
+
+//create new user
 router.post('/', async (req, res) => {
   try {
-    const userData = await User.create(req.body);
+    const userInput ={
+      name: req.body.name,
+      email: req.body.email,
+      password: req.body.password
+    };
+
+    const dbUserData = await User.create(userInput);
+    res.status(200).json(dbUserData);
 
     req.session.save(() => {
-      req.session.user_id = userData.id;
-      req.session.logged_in = true;
-
-      res.status(200).json(userData);
+      req.session.loggedIn = true;
     });
+
   } catch (err) {
-    res.status(400).json(err);
+    console.log(err);
+    res.status(500).json(err);
   }
 });
 
-router.post('/', async (req, res) => {
+//Login
+router.post('/login', async (req, res) => {
   try {
-    const userData = await User.findOne({ where: { UserName: req.body.UserName } });
-
-    if (!userData) {
+    const dbUserData = await User.findOne({ where: { email: req.body.email } });
+    if (!dbUserData) {
       res
         .status(400)
-        .json({ message: 'Incorrect Username or password, please try again' });
+        .json({ message: 'Incorrect Email or password, please try again' });
       return;
     }
 
-    const validPassword = await userData.checkPassword(req.body.password);
+    const validPassword = await dbUserData.checkPassword(req.body.password);
 
     if (!validPassword) {
       res
         .status(400)
-        .json({ message: 'Incorrect user or password, please try again' });
+        .json({ message: 'Incorrect Email or password, please try again' });
       return;
     }
 
     req.session.save(() => {
-      req.session.user_id = userData.id;
-      req.session.logged_in = true;
-      
-      res.json({ user: userData, message: 'You are now logged in!' });
-    });
+      req.session.loggedIn = true;
 
+      res
+        .status(200)
+        .json({ user: dbUserData, message: 'You are now logged in!' });
+    });
   } catch (err) {
     res.status(400).json(err);
   }
 });
 
 // router.post('/logout', (req, res) => {
-//   if (req.session.logged_in) {
+//   if (req.session.loggedIn) {
 //     req.session.destroy(() => {
 //       res.status(204).end();
 //     });
